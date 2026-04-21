69e7b89d3d5e6cd9a5ade881857eb52dd7de66f6
= <=>
2026-04-21 18:51:22 -0300

    sad

--- src/modules/order/data-source.order.ts
+++ src/modules/order/data-source.order.ts
@@ -13,7 +13,7 @@ export default class OrderDB implements InterfaceModuleDB {
             port: Number(process.env.DB_PORT) || 5432,
             username: process.env.DB_USERNAME!,
             password: process.env.DB_PASSWORD!,
-            database: process.env.DB_ORDER_NAME!,
+            database: process.env.DB_AUTH_NAME!,
             synchronize: true,
             logging: true,
             entities: [Order, OrderHistory],
